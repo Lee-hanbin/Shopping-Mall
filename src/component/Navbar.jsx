4044383515e3e8069eb0ae8169f0041c9b0a1e59
Lee-hanbin <hanbin0501@gmail.com>
2023-03-14 10:23:54 +0900

    ⭐ FEAT : React Query 이용해서 ajax 요청 실시간 관리

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,8 +1,26 @@
 import { Container, Nav, Navbar } from "react-bootstrap"
 import { useNavigate } from "react-router-dom";
+import axios from 'axios';
+import { useQuery } from 'react-query';
+
 
 function NavbarUi() {
   let navigate = useNavigate();       // 페이지 이동 도와주는 함수
+
+
+  // axios 요청을 useQuery 감싸서 사용
+  let res = useQuery('getName', () => {
+    return axios.get('https://codingapple1.github.io/userdata.json')
+    .then((a) => { 
+      console.log('요청됨')
+      return a.data
+    })
+  },{ staleTime : 2000 })
+  
+  // res.data // ajax요청이 성공시 데이터
+  // res.isLoading // 로딩중인지 쉽게 파악 가능
+  // res.error // 실패시 true
+
 
   return (
       <Navbar bg="dark" variant="dark">
@@ -11,9 +29,15 @@ function NavbarUi() {
           <Nav className="me-auto">
             <Nav.Link onClick={()=>{navigate('/')}}>Home</Nav.Link>
             <Nav.Link onClick={()=>{navigate('/cart')}} >Cart</Nav.Link>
+            <Nav.Link onClick={() => {navigate('/watched')}}>최근 본 상품</Nav.Link>
           </Nav>
-          <Nav>
-            <Nav.Link style={{ textAlign: "right" }} onClick={() => {navigate('/watched')}}>최근 본 상품</Nav.Link>
+          {/* <Nav style={{ color: "white"}} >반가워요 Lee</Nav> */}
+          <Nav style={{ color: "white"}}>
+            {/* { res.isLoading ? 'loading...' : res.data.name} */}
+            { res.isLoading && 'loading...'}
+            { res.error && 'error...'}
+            { res.data && res.data.name}
+            
           </Nav>
         </Container>
       </Navbar>
